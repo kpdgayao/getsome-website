69235d555c3fbe0f1339fe6ded12868948ef84f0
KP <88073998+kpdgayao@users.noreply.github.com>
2024-12-03 16:30:56 +0800

    Add main.tsx entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import '@fontsource/playfair-display/700.css'
-import '@fontsource/open-sans/400.css'
-import '@fontsource/open-sans/300.css'
+import Home from './pages/index'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
