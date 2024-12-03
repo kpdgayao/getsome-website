863103138e2cbd8e6ed9276046c2cafd320e87e6
KP <88073998+kpdgayao@users.noreply.github.com>
2024-12-03 16:13:46 +0800

    Add main.tsx entry point

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import '@fontsource/playfair-display/700.css'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/300.css'
+import './styles/globals.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
